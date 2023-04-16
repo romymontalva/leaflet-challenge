7f6e96589bda5bb9cb313414cd349497955eb1b5
romymontalva <romymontalva2010@gmail.com>
2023-04-16 01:11:10 -0400

    all done

--- part-1/static/js/logic.js
+++ part-1/static/js/logic.js
@@ -1,3 +1,22 @@
-alert("Hello")
 
+
+let basmap = L.tileLayer(
+    "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png", {
+    attribution: 'Map data: &copy; '
+});
+
+let map = L.map("map", {
+    center: [ 40.7, -94.5],
+    zoom: 3
+})
+
+bassmap.addTo(map);
+
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+    console.log(data);
+    L.geojson(data).addTo(map);
+    
+
+
+})
 
